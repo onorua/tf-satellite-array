188767ed11881a178ef128137b01c92761617d9f
Yaroslav Molochko <y.molochko@anchorfree.com>
2017-05-19 09:35:20 -0700

    Revert "Try not overcomplicate, maybe tf will allow scaledown"
    This reverts commit e115a21ef2d5c2071fdeb54ecf987c48c69c2e6b.

--- s3.tf
+++ s3.tf
@@ -1,39 +1,19 @@
 # S3 - Hostfile Storage
-resource "aws_s3_bucket_object" "do_hosts" {
+resource "aws_s3_bucket_object" "host_ips_file" {
     bucket = "tf_af_hostarray"
     key = "hosts/${var.name_prefix}"
-    content = "${join("\n",formatlist("%v   %v", digitalocean_droplet.node.*.ipv4_address, digitalocean_droplet.node.*.name))}\n"
+    content = "${join("\n",formatlist("%v   %v", concat(digitalocean_droplet.node.*.ipv4_address, aws_instance.node.*.public_ip), concat(digitalocean_droplet.node.*.name, aws_instance.node.*.tags.Name)))}\n"
     content_type = "text/plain"
-    etag = "${md5("${join("\n",formatlist("%v   %v", digitalocean_droplet.node.*.ipv4_address, digitalocean_droplet.node.*.name))}\n")}"
+    etag = "${md5("${join("\n",formatlist("%v   %v", concat(digitalocean_droplet.node.*.ipv4_address, aws_instance.node.*.public_ip), concat(digitalocean_droplet.node.*.name, aws_instance.node.*.tags.Name)))}\n")}"
     count  = "${lookup(map("do", "${signum(var.nodes)}"), "${var.provider}" , "0")}"
 }
 
 # S3 - Hostfile Storage for private IPs
-resource "aws_s3_bucket_object" "do_hosts_private" {
+resource "aws_s3_bucket_object" "host_private_ips_file" {
     bucket = "tf_af_hostarray"
     key = "hosts_private/${var.name_prefix}"
-    content = "${join("\n",formatlist("%v %v", digitalocean_droplet.node.*.ipv4_address_private, digitalocean_droplet.node.*.name))}\n"
+    content = "${join("\n",formatlist("%v %v", concat(digitalocean_droplet.node.*.ipv4_address_private, aws_instance.node.*.private_ip), concat(digitalocean_droplet.node.*.name, aws_instance.node.*.tags.Name)))}\n"
     content_type = "text/plain"
-    etag = "${md5("${join("\n",formatlist("%v %v", digitalocean_droplet.node.*.ipv4_address_private, digitalocean_droplet.node.*.name))}\n")}"
+    etag = "${md5("${join("\n",formatlist("%v %v", concat(digitalocean_droplet.node.*.ipv4_address_private,aws_instance.node.*.private_ip), concat(digitalocean_droplet.node.*.name, aws_instance.node.*.tags.Name)))}\n")}"
     count  = "${lookup(map("do", "${signum(var.nodes)}"), "${var.provider}", "0")}"
-}
-
-# S3 - Hostfile Storage
-resource "aws_s3_bucket_object" "aws_hosts" {
-    bucket = "tf_af_hostarray"
-    key = "hosts/${var.name_prefix}"
-    content = "${join("\n",formatlist("%v   %v", aws_instance.node.*.public_ip, aws_instance.node.*.tags.Name))}\n"
-    content_type = "text/plain"
-    etag = "${md5("${join("\n",formatlist("%v   %v", aws_instance.node.*.public_ip, aws_instance.node.*.tags.Name))}\n")}"
-    count  = "${lookup(map("aws", "${signum(var.nodes)}"), "${var.provider}" , "0")}"
-}
-
-# S3 - Hostfile Storage for private IPs
-resource "aws_s3_bucket_object" "aws_hosts_private" {
-    bucket = "tf_af_hostarray"
-    key = "hosts_private/${var.name_prefix}"
-    content = "${join("\n",formatlist("%v %v", aws_instance.node.*.private_ip, aws_instance.node.*.tags.Name))}\n"
-    content_type = "text/plain"
-    etag = "${md5("${join("\n",formatlist("%v %v", aws_instance.node.*.private_ip, aws_instance.node.*.tags.Name))}\n")}"
-    count  = "${lookup(map("aws", "${signum(var.nodes)}"), "${var.provider}" , "0")}"
 }
